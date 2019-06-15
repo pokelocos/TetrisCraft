76cb799aff6e7f59feab16554fa664fe5e6e6b3c
Santiago <santiago.cervera117@gmail.com>
2019-06-15 19:32:11 -0400

    Changes in game.js

--- Code/Game.js
+++ Code/Game.js
@@ -4,29 +4,35 @@ class Game
     {
         this.shape;
         this.nextShape;
-        this.world;
+        this.world = new World(7,7,12);
         
 		this.scene = new THREE.Scene();
 		this.camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
         this.camera.position.z = 1;
-        this.camera.setUp();
+        //this.camera.setUp();
 
         this.scene.add(this.camera);
 
-        this.renderer = new THREE.WebGLRenderer();
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        document.body.appendChild(renderer.domElement);
+        this.renderer = new THREE.WebGLRenderer({antialias: true});
+        //renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(this.renderer.domElement);
     }
 
     Update()
     {
-        world.Update();
-        shape.Update();
+        this.world.Update();
+        this.shape.Update();
     }
 
     Draw()
     {
         this.renderer.render(this.scene,this.camera);
+
+		//var size = 10;
+		//var divisions = 10;
+
+		//var gridHelper = new THREE.GridHelper( size, divisions );
+		//scene.add( gridHelper );
     }
 
     SpawnShape()
