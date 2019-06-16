5e1099f4bb96ddad7f52e9fd7e45d8ea0e1a37a1
Nicolas Romero <nare.rare.fire@gmail.com>
2019-06-15 21:40:41 -0400

    sube tus cambios

--- Code/TetrisCraft.js
+++ Code/TetrisCraft.js
@@ -1,6 +1,6 @@
 var camera, scene, renderer;
 var geometry, material, mesh;
-var state = 0;
+var state = 1;
 
 //Scenes
 var game,main,instructions,options; // esto se puede cambiar a lista
@@ -12,17 +12,13 @@ function init()
 {
 	game = new Game();
 	main = new MainMenu();
-<<<<<<< HEAD
 	instructions = new Instructions();
 	options = new Options();	
 	
-	this.renderer = new THREE.WebGLRenderer();
-        //renderer.setSize(window.innerWidth, window.innerHeight);
+	this.renderer = new THREE.WebGLRenderer({antialias:true});
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
 	document.body.appendChild(this.renderer.domElement);
-=======
-	//instructions = new Instructions();
-	//options = new Options();	
->>>>>>> 249f7f8b7d38b3e99d21495031c62fb98f0e4b70
+
 }
 
 function loop() 
@@ -32,19 +28,19 @@ function loop()
 	{
 		case 0:
 			main.Update();
-			main.Draw(this.render);
+			main.Draw(this.renderer);
 			break;
 		case 1:
 			game.Update();
-			game.Draw(this.render);
+			game.Draw(this.renderer);
 			break;
 		case 2:
 			instructions.Update();
-			instructions.Draw(this.render);
+			instructions.Draw(this.renderer);
 			break;
 		case 2:
 			options.Update();
-			Option.Draw(this.render);
+			Option.Draw(this.renderer);
 			break;
 		default:
 			Conosole.Log("[Scene 'id' error, id: "+state+" not found]");
